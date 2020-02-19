3e024cede3ed4cf564c0ae4867dd7d2dfe4edfd7
Angel Eduardo Gonzalez <agonzalez@asw.edu.co>
2020-02-19 09:17:20 -0500

    actualizacion de la vista agendamiento se adicionan nuevos estados

--- src/components/Agenda/AgendaView.vue
+++ src/components/Agenda/AgendaView.vue
@@ -9,7 +9,10 @@
                 <v-row>
                     <v-col cols="12" md="12">
                         <v-select v-model="estado" :items="estados" label="Estado"></v-select>
-
+                    </v-col>
+                </v-row>
+                <v-row v-if="estado == 'Agendamiento'">
+                    <v-col cols="12" md="6">
                         <v-select v-if="estado == 'Agendamiento'" v-model="nueva_cita.sede" :items="sedes" label="Sede"
                             item-text="text" item-value="id">
                         </v-select>
@@ -25,16 +28,36 @@
                             item-text="text" item-value="id"></v-select>
                     </v-col>
                 </v-row>
+                <v-row v-if="estado == 'Dato errado'">
+                    <v-col cols="12" md="12">
+                        <v-textarea label="Observación" v-model="nueva_cita.observación"></v-textarea>
 
+                    </v-col>
 
-
+                </v-row>
+                <v-row v-if="estado == 'Llamar despues'">
+                    <v-col cols="12" md="12">
+                        <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date"
+                            transition="scale-transition" offset-y min-width="290px">
+                            <template v-slot:activator="{ on }">
+                                <v-text-field v-model="date" label="Fecha proxima llamada" prepend-icon="event"
+                                    v-on="on"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="nueva_cita.fecha_proxima_llamada" no-title scrollable>
+                                <v-spacer></v-spacer>
+                                <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                                <v-btn text color="primary" @click="$refs.menu.save(nueva_cita.fecha_proxima_llamada)">OK</v-btn>
+                            </v-date-picker>
+                        </v-menu>
+                    </v-col>
+                </v-row>
 
             </v-form>
 
         </v-card-text>
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="red darken-1" text @click="agendar">Agendar</v-btn>
+            <v-btn color="red darken-1" text @click="agendar">Guardar</v-btn>
             <v-btn color="blue darken-1" text @click="cerrar">Cerrar</v-btn>
         </v-card-actions>
     </v-card>
@@ -49,13 +72,17 @@
     export default {
         name: 'AgendaView',
         data: () => ({
-            estados: ['Agendamiento'],
+            menu: false,
+            date: new Date().toISOString().substr(0, 10),
+            estados: ['Agendamiento', 'No contesta', 'Dato errado', 'Llamar despues', 'Sin sede en ciudad'],
             estado: null,
             nueva_cita: {
                 sede: null,
                 fecha: null,
                 hora: null,
-                id: null
+                id: null,
+                observación: null,
+                fecha_proxima_llamada:null
             },
             fechas: [],
             sedes: [],
