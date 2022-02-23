f68f14fb43f5b649a3901815073a7706a46dc10c
= <=>
2022-02-23 11:34:33 -0500

    Quitar ver citas a coordinadores de admisiones

--- src/views/Baseline.vue
+++ src/views/Baseline.vue
@@ -216,7 +216,7 @@
                                 <v-list-item-title>Hits</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
-                        <v-list-item v-if="isLogged" link @click="dirigir('/Reportes/Citas')">
+                        <v-list-item v-if="isLogged && user.data.grupo_id != 26" link @click="dirigir('/Reportes/Citas')">
                             <v-list-item-action>
                                 <v-icon>filter_list</v-icon>
                             </v-list-item-action>
