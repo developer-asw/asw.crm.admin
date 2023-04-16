63e4a0ce8d6cdb551d319f27e6ed4116cd053b41
Lenier Diaz <lpachecod@asw.edu.co>
2023-04-16 12:39:16 -0500

    Agregar url de sandbox CRM

--- src/modules/config.js
+++ src/modules/config.js
@@ -4,11 +4,16 @@ const dev_enviroment = {
 const production_enviroment = {
   ROOT_API: 'https://crm.asw.edu.co/api/admin/',
 };
+const qa_enviroment = {
+  ROOT_API: 'https://sandbox-crm.asw.edu.co/api/admin/',
+};
 
 let enviroment;
 
 if(process.env.NODE_ENV === 'production'){
 	enviroment = production_enviroment;	
+}else if(process.env.NODE_ENV === 'qa'){
+	enviroment = qa_enviroment;	
 }else{
 	enviroment = dev_enviroment;	
 }
