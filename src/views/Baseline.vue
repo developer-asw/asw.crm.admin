8619401b455edd88cc43aba2c81768728c5c516f
Lenier Diaz <lpachecod@asw.edu.co>
2023-08-24 10:57:08 -0500

    Agregar marca de agua en asignar tarea

--- src/views/Baseline.vue
+++ src/views/Baseline.vue
@@ -387,7 +387,7 @@
 
         <v-footer color="blue-grey" class="white--text text-center">
             <v-card-text>
-                American School Way &copy; 2023 - v.1.4.0
+                American School Way &copy; 2023 - v.1.4.1
             </v-card-text>
         </v-footer>
         
